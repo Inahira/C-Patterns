05092e62b7005b2e376b556804d92d5437de1cf4
Tahira inam <sololearner120@gmai.com>
2024-03-16 09:20:51 -0700

    initial commit

---
+++ upgraded Diamond.cpp
@@ -0,0 +1,22 @@
+/*
+#include <iostream>
+using namespace std;
+
+void rrtriangle(int i, int j)
+{
+	for (i=1; i<=3; i++)
+	{
+		for (j=3; j>=i; j--)
+		{
+			cout<<"* ";
+		}
+		cout<<"\n";
+	}
+}
+
+int main()
+{
+	rrtriangle( i, j);
+	return 0;
+}
+*/
